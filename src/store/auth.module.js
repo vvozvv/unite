c8b0a525cfbc596efc0734f47acc0a2609cf20b5
vvozvv <a.vvozvv@gmail.com>
2021-06-18 10:47:11 +0300

    change form-icon

--- src/store/auth.module.js
+++ src/store/auth.module.js
@@ -46,43 +46,45 @@ const actions = {
     commit('SET_USER', payload)
   },
   login({ commit }, condidate) {
+    commit('CLEAR_ERROR')
     return new Promise((resolve, reject) => {
       ApiService.post('/auth/local', condidate)
-      .then(({ data }) => {
-        commit('SET_USER', data.user)
-        setToken(data.jwt)
-        resolve(data)
-      })
-      .catch((error) => {
-        commit('SET_ERROR', getError(error.response))
-        reject(error)
-      })
+        .then(({ data }) => {
+          commit('SET_USER', data.user)
+          setToken(data.jwt)
+          resolve(data)
+        })
+        .catch((error) => {
+          commit('SET_ERROR', getError(error.response))
+          reject(error)
+        })
     })
   },
   register({ commit }, condidate) {
+    commit('CLEAR_ERROR')
     return new Promise((resolve, reject) => {
       ApiService.post('/auth/local/register', condidate)
-      .then(({ data }) => {
-        commit('SET_USER', data.user)
-        setToken(data.jwt)
-        resolve(data)
-      })
-      .catch((error) => {
-        commit('SET_ERROR', getError(error.response, 'register'))
-        reject(error)
-      })
+        .then(({ data }) => {
+          commit('SET_USER', data.user)
+          setToken(data.jwt)
+          resolve(data)
+        })
+        .catch((error) => {
+          commit('SET_ERROR', getError(error.response, 'register'))
+          reject(error)
+        })
     })
   },
   checkAuth({ commit }) {
     if (getToken() && getUserId() !== 'undefined') {
       ApiService.setHeader()
       ApiService.get('users', getUserId())
-      .then(({ data }) => {
-        commit('SET_USER', data)
-      })
-      .catch(({ response }) => {
-        commit('SET_ERROR', response.data.errors)
-      })
+        .then(({ data }) => {
+          commit('SET_USER', data)
+        })
+        .catch(({ response }) => {
+          commit('SET_ERROR', response.data.errors)
+        })
     } else {
       commit('PURGE_AUTH')
     }
